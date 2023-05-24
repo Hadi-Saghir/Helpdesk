8f2b7749dad602b42b0ca376c34e0cb195023a58
Hadi-Saghir <89988015+Hadi-Saghir@users.noreply.github.com>
2023-05-25 00:43:52 +0200

    prefix generator
    For the script to work there need to generate href to where the user store their file. This will allow the user to generate it automatically. Instructions  need to be provided on how to use this generator or further modifications to automate it fully

---
+++ Prefix_Generator.ps1
@@ -0,0 +1,19 @@
+﻿# Define a helper function to set clipboard data
+function Set-ClipboardData {
+    param (
+        [string]$htmlData,
+        [string]$plainTextData
+    )
+
+    [System.Windows.Forms.Clipboard]::Clear()
+    $dataObject = New-Object System.Windows.Forms.DataObject
+    $dataObject.SetData("HTML Format", $htmlData)
+    $dataObject.SetData("Text", $plainTextData)
+    [System.Windows.Forms.Clipboard]::SetDataObject($dataObject)
+}
+
+# Get the current clipboard content
+$clipboardContent = [System.Windows.Forms.Clipboard]::GetText("Html")
+
+# Set the modified text back to the clipboard as HTML format
+Set-ClipboardData $clipboardContent $clipboardContent
